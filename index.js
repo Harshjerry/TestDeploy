024da10d5112de157a33d9d08d8e69b5f5d8487d
harsh <harshdeep1043@gmail.com>
2024-03-06 12:10:55 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const promptRouter = require('./routes/promptRoute');
 const cors = require('cors');
-
 dotenv.config();
 
 mongoose.connect(process.env.CONN_STR, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,7 +15,13 @@ mongoose.connect(process.env.CONN_STR, { useNewUrlParser: true, useUnifiedTopolo
 app.use(cors());
 app.use(express.json());
 app.use("/api/prompt", promptRouter);
-
-app.listen(5000, () => {
+app.use("/",(req,res)=>{
+  return res.status(200).json(
+    {success:true,
+    message:" ia am working fine"}
+  )
+})  
+app.listen(process.env.PORT, () => {
   console.log("backend is running");
 });
+
