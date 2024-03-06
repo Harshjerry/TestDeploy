b35e1d64f7defebe592814ebe49d3eafe3e684e5
harsh <harshdeep1043@gmail.com>
2024-03-06 12:29:13 +0530

    change ini env

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const promptRouter = require('./routes/promptRoute');
 const cors = require('cors');
+const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(process.env.CONN_STR, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("db connection successful"))
   .catch((err) => {
     console.log(err);
   });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());   
+
+
 app.use("/api/prompt", promptRouter);
 app.use("/",(req,res)=>{
   return res.status(200).json(
